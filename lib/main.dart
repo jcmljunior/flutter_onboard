0107ea03e5d97f79465a12ad71a09051bf2ea5dd
Julio Cesar <165454530+jcmljunior@users.noreply.github.com>
2024-09-24 08:30:42 -0300

    alteração na estrategia de url no flutter web.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:flutter_onboard/features/app/widgets/app.widget.dart';
 
 void main() {
+  usePathUrlStrategy();
   runApp(const AppWidget());
 }
